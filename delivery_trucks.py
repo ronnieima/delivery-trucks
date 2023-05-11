45eab89b52025775b3c9ce168b5dc9332a648962
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-05-11 11:49:14 +0200

    update hash function

--- delivery_trucks.py
+++ delivery_trucks.py
@@ -3,8 +3,12 @@ class Hashmap:
         self.arrSize = arrSize
         self.array = [None] * arrSize
 
-    def hasher(key):
-        return key % 10
+    #hash formula: (ASCII value of character * 2) % size of the array
+    def hasher(self, key):
+        hash = 0
+        for char in key:
+            hash += ord(char) * 2
+        return hash % self.arrSize
 
 
 
