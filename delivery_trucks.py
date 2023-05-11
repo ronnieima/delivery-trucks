c7d3588e74b6c49180123cc0b2a4bb61cbb48611
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-05-11 11:56:27 +0200

    create addKVPair and searchKey functions

--- delivery_trucks.py
+++ delivery_trucks.py
@@ -1,15 +1,22 @@
 class Hashmap:
-    def __init__(self, arrSize):
-        self.arrSize = arrSize
-        self.array = [None] * arrSize
+    def __init__(self, arraySize):
+        self.arraySize = arraySize
+        self.array = [None] * arraySize
 
     #hash formula: (ASCII value of character * 2) % size of the array
     def hasher(self, key):
         hash = 0
         for char in key:
             hash += ord(char) * 2
-        return hash % self.arrSize
+        return hash % self.arraySize
 
+    def addKVPair(self, key, value):
+        hash = self.hasher(key)
+        self.array[hash] = value
+
+    def searchKey(self, key):
+        hash = self.hasher(key)
+        return self.array[hash]
 
 
 
